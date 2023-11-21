87b79edb200979b334fc546e0cc2c46bc216a49b
vijaygowdu <vijaygowdu12@gmail.com>
2023-11-21 13:59:39 +0530

    add

--- snmp/webserver.py
+++ snmp/webserver.py
@@ -1,27 +1,3 @@
-# EX01 Developing a Simple Webserver
-## Date:
-
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
-```
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 content='''
@@ -48,17 +24,6 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(content.encode())
 
 print("This is my webserver") 
-server_address =('',80)
+server_address =('',8000)
 httpd = HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-```
-
-## OUTPUT:
-## serveroutput
-![Alt text](<snmp/Screenshot 2023-11-21 135158.png>)
-
-## clientoutput
-![Alt text](<snmp/Screenshot 2023-11-21 135227.png>)
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
